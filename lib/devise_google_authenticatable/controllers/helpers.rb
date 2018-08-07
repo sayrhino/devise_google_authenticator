692a4becb71134f83ca0582854937c99107f72a5
Aleksey Popov <dazerok@gmail.com>
2018-08-07 16:25:32 +0300

    remove escaping for qr code generation

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -9,7 +9,6 @@ module DeviseGoogleAuthenticator
         app = user.class.ga_appname || Rails.application.class.parent_name
         data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
         data << "&issuer=#{issuer}" unless issuer.nil?
-        data = Rack::Utils.escape(data)
         qrcode = RQRCode::QRCode.new(data, level: :m, mode: :byte_8bit)
         png = qrcode.as_png(fill: 'white', color: 'black', border_modules: 1, module_px_size: 4)
         url = "data:image/png;base64,#{Base64.encode64(png.to_s).strip}"
